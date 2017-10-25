7534a745fc5b03894e0d50bae96be0ae6e2b6731
Patricio Palladino <email@patriciopalladino.com>
2017-10-25 17:46:25 -0300

    Initial version.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # truffle-flattener
-Truffle Flattener concats solidity files developed under Truffle with all of their dependencies
+
+Truffle Flattener concats solidity files developed under Truffle with all of
+their dependencies.
+
+# Installation
+
+`npm install --save-dev truffle-flattener`
+
+# Usage
+
+Just intall it with npm in your traffle project and run
+`traffle-flattener <solidity-files>`.
+
+# Why not [Solidity Flattener](https://github.com/BlockCatIO/solidity-flattener)?
+
+This project is a [Truffle](https://github.com/trufflesuite/truffle) specific
+reimplementation of Solidity Flattener. By being closely couple to Truffle it
+can take advantage of its dependencies resolution logic making `--solc-paths` a
+thing of the past. It also supports flattening more than one file at once,
+concatenating everything in the right order, whithout duplicating any file.
---
+++ index.js
@@ -0,0 +1,217 @@
+#! /usr/bin/env node
+const process = require("process");
+const fs = require("fs");
+const path = require("path");
+
+const semver = require("semver");
+const Config = require("truffle-config");
+const Resolver = require("truffle-resolver");
+const tsort = require("tsort");
+const SolidityParser = require("solidity-parser");
+
+const PRAGAMA_SOLIDITY_VERSION_REGEX = /^\s*pragma\ssolidity\s+(.*?)\s*;/;
+const SUPPORTED_VERSION_DECLARATION_REGEX = /^\^?\d+(\.\d+){1,2}$/;
+
+function unique(array) {
+  return [...new Set(array)];
+}
+
+function resolve(importPath) {
+  const config = Config.default();
+  const resolver = new Resolver(config);
+
+  return new Promise((resolve, reject) => {
+    resolver.resolve(importPath, (err, fileContents, filePath) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({ fileContents, filePath });
+    });
+  });
+}
+
+function getDependencies(filePath, fileContents) {
+  const dependencies = [];
+
+  let imports;
+  try {
+    imports = SolidityParser.parse(fileContents, "imports");
+  } catch (error) {
+    throw new Error(
+      "Could not parse " + filePath + " for extracting its imports."
+    );
+  }
+
+  for (let dependency of imports) {
+    if (dependency.startsWith("./") || dependency.startsWith("../")) {
+      dependency =
+        filePath.substring(0, filePath.lastIndexOf("/") + 1) + dependency;
+      dependency = path.normalize(dependency);
+    }
+
+    dependencies.push(dependency);
+  }
+
+  return dependencies;
+}
+
+async function dependenciesDfs(graph, visitedFiles, filePath) {
+  visitedFiles.push(filePath);
+
+  const resolved = await resolve(filePath);
+
+  const dependencies = getDependencies(
+    resolved.filePath,
+    resolved.fileContents
+  );
+
+  for (let dependency of dependencies) {
+    graph.add(dependency, filePath);
+
+    const resolvedDependency = await resolve(dependency);
+
+    if (!visitedFiles.includes(dependency)) {
+      await dependenciesDfs(graph, visitedFiles, dependency);
+    }
+  }
+}
+
+async function getSortedFilePaths(entryPoints) {
+  const graph = tsort();
+  const visitedFiles = [];
+
+  for (const entryPoint of entryPoints) {
+    await dependenciesDfs(graph, visitedFiles, entryPoint);
+  }
+
+  const topologicalSortedFiles = graph.sort();
+
+  // If an enrty has no dependency it won't be included in the graph, so we
+  // add them and then dedup the array
+  const withEntries = topologicalSortedFiles.concat(entryPoints);
+
+  const files = unique(withEntries);
+
+  return files;
+}
+
+async function printFileWithoutPragma(filePath) {
+  const resolved = await resolve(filePath);
+  const output = resolved.fileContents.replace(
+    PRAGAMA_SOLIDITY_VERSION_REGEX,
+    ""
+  );
+
+  console.log(output.trim());
+}
+
+async function getFileCompilerVersionDeclaration(filePath) {
+  const resolved = await resolve(filePath);
+
+  const matched = resolved.fileContents.match(PRAGAMA_SOLIDITY_VERSION_REGEX);
+
+  if (matched === null) {
+    return undefined;
+  }
+
+  const version = matched[1];
+
+  if (!SUPPORTED_VERSION_DECLARATION_REGEX.test(version)) {
+    throw new Error(
+      "Unsupported compiler version declaration in  " +
+        filePath +
+        ": " +
+        version +
+        ". Only pinned or ^ versions are supported."
+    );
+  }
+
+  return version;
+}
+
+async function normalizeCompilerVersionDeclarations(files) {
+  let pinnedVersion;
+  let pinnedVersionFile;
+
+  let maxCaretVersion;
+  let maxCaretVersionFile;
+
+  for (const file of files) {
+    const version = await getFileCompilerVersionDeclaration(file);
+
+    if (version === undefined) {
+      continue;
+    }
+
+    if (version.startsWith("^")) {
+      if (maxCaretVersion == undefined) {
+        maxCaretVersion = version;
+        maxCaretVersionFile = file;
+      } else {
+        if (semver.gt(version.substr(1), maxCaretVersion.substr(1))) {
+          maxCaretVersion = version;
+          maxCaretVersionFile = file;
+        }
+      }
+    } else {
+      if (pinnedVersion === undefined) {
+        pinnedVersion = version;
+        pinnedVersionFile = file;
+      } else if (pinnedVersion !== version) {
+        throw new Error(
+          "Differernt pinned compiler versions in " +
+            pinnedVersionFile +
+            " and " +
+            file
+        );
+      }
+    }
+
+    if (maxCaretVersion !== undefined && pinnedVersion !== undefined) {
+      if (!semver.satisfies(pinnedVersion, maxCaretVersion)) {
+        throw new Error(
+          "Incompatible compiler version declarations in " +
+            maxCaretVersionFile +
+            " and " +
+            pinnedVersionFile
+        );
+      }
+    }
+  }
+
+  if (pinnedVersion !== undefined) {
+    return pinnedVersion;
+  }
+
+  return maxCaretVersion;
+}
+
+async function printContactenation(files) {
+  const version = await normalizeCompilerVersionDeclarations(files);
+
+  if (version) {
+    console.log("pragma solidity " + version + ";");
+  }
+
+  for (const file of files) {
+    console.log("\n// File: " + file + "\n");
+    await printFileWithoutPragma(file);
+  }
+}
+
+async function main(files) {
+  if (files.length == 0) {
+    console.error("Usage: truffle-flattener <files>");
+  }
+
+  try {
+    const sortedFiles = await getSortedFilePaths(files);
+    await printContactenation(sortedFiles);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main(process.argv.slice(2));
