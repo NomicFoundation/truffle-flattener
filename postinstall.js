a8829dae89aea765500f7c83594d6c6c76b7c11a
Patricio Palladino <email@patriciopalladino.com>
2018-10-04 12:17:49 -0300

    Remove Buidler message

--- postinstall.js
+++
@@ -1,5 +0,0 @@
-const chalk = require("chalk");
-
-console.log(chalk.green("Check out Buidler, our alternative to Truffle. It's got flattening built-in, it's faster, and much more flexible."));
-console.log(chalk.green("Learn more at https://github.com/nomiclabs/buidler"));
-console.log();
